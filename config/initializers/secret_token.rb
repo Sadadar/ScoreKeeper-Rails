7a5ef71a0547c38a796f1910ac249b72b101a9c0
Matt Greenberg <matt@connectsolutions.com>
2012-12-30 13:15:49 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScoreKeeperRails::Application.config.secret_token = '3dfe6292f876742ec374a92d40fc08c6c45cd8b023040ed16528a1f6de454bf2582a992fec184c334b568454ad7f65c5ed88e3509a33a475ba3188ac661b9725'
